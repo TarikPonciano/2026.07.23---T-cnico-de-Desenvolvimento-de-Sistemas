3841112be5e14fe354eab0b2d3a86c6755551eff
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-10 18:17:58 -0300

    Códigos Aula 3

---
+++ Aula 3/introducao.py
@@ -0,0 +1,23 @@
+#Imprima na tela uma saudação para uma pessoa X e abaixo imprima o nome, a idade e a altura dessa pessoa
+
+#Olá, Fulano
+#Nome: Fulano
+#Idade: 23 anos
+#Altura: 1.75m
+
+print("Olá, Jorel")
+print("------------------")
+print("Nome: Jorel")
+print("Idade: 20 anos")
+print("Altura: 1.80m")
+
+# print("Olá, Jorel\nNome: Jorel")
+
+# print("""
+# Olá, Jorel
+      
+# Nome: Jorel
+      
+# -----------
+
+# """)
---
+++ Aula 3/variaveis.py
@@ -0,0 +1,16 @@
+# nome = "Tarik Ponciano"
+
+# # Força Bruta
+# # print("Olá,",nome+"!")
+
+# # String Formatada
+# print(f"Olá, {nome}!")
+
+# Crie um programa que contém as variáveis nome e idade. E exiba na tela uma mensagem que dá boas vindas à um pessoa e imprime sua idade. Exemplo
+
+# Olá, {nome}. Você tem {idade} anos de idade!
+
+nome = "Vovó Juju"
+idade = 67
+
+print(f"Olá, {nome}. Você tem {idade} anos de idade!")
