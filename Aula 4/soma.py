ffe76943b241b443714ea7d311907a4efec6d2b8
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-10 20:01:23 -0300

    Atividades

--- Aula 4/soma.py
+++ Aula 4/soma.py
@@ -4,3 +4,9 @@
 #Digite o número 1: 20
 #Digite o número 2: 30
 #Resultado da Soma: 50
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+soma = num1 + num2
+
+print(f"Resultado da soma: {soma}")
+#Melhore o programa para exibir a subtração, a multiplicação e a divisão dos números
---
+++ Aula 4/somamelhor.py
@@ -0,0 +1,21 @@
+# Construa um programa que recebe dois números e imprime a soma desses números. 
+# Melhore o programa para exibir a subtração, a multiplicação e a divisão dos números
+
+print("Bem vindo à calculadora XYZ")
+
+num1 = float(input("Digite o primeiro número:"))
+
+num2 = float(input("Digite o segundo número:"))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"Resultado da Soma: {soma}")
+print(f"Resultado da Subtração: {subtracao}")
+print(f"Resultado da Multiplicação: {multiplicacao}")
+print(f"Resultado da Divisão: {divisao}")
+
+# print(f"Resultados")
+# print(f"{num1} + {num2} = {soma}")
