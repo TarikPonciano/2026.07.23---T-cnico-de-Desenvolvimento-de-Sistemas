2c88a7f2799f8be5003eb2ea80c82bcc3e3bc45d
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-10 20:12:18 -0300

    Create exercicios.py

---
+++ Aula 4/exercicios.py
@@ -0,0 +1,5 @@
+# Crie um programa que pede o nome da pessoa e o ano de nascimento da pessoa. Exiba o nome e a idade da pessoa.
+
+# Crie um programa que recebe o nome de um produto, o preço e a quantidade comprada desse produto. Exiba ao final o valor total a pagar desse produto.
+
+# Crie um programa que pede o nome de um aluno e suas 4 notas. Ao final exiba a média do aluno.
