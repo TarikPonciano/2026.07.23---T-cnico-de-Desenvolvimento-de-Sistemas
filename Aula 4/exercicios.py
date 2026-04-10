3967c452432285088cbf2000019f2f190dd3d0d5
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-10 20:42:52 -0300

    Update exercicios.py

--- Aula 4/exercicios.py
+++ Aula 4/exercicios.py
@@ -1,5 +1,14 @@
 # Crie um programa que pede o nome da pessoa e o ano de nascimento da pessoa. Exiba o nome e a idade da pessoa.
 
+nome = input("Digite o seu nome:")
+ano_nascimento = int(input("Digite o ano em que nasceu:"))
+ano_atual = 2026
+
+idade = ano_atual - ano_nascimento
+
+print(f"Olá, {nome}!")
+print(f"Você tem {idade} anos.")
+
 # Crie um programa que recebe o nome de um produto, o preço e a quantidade comprada desse produto. Exiba ao final o valor total a pagar desse produto.
 
 # Crie um programa que pede o nome de um aluno e suas 4 notas. Ao final exiba a média do aluno.
