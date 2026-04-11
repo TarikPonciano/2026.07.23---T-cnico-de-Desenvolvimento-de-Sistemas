883058392e0dbc4d14e7788915c8100a60ec678e
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-10 21:04:24 -0300

    Update exercicios.py

--- Aula 4/exercicios.py
+++ Aula 4/exercicios.py
@@ -1,14 +1,22 @@
 # Crie um programa que pede o nome da pessoa e o ano de nascimento da pessoa. Exiba o nome e a idade da pessoa.
 
-nome = input("Digite o seu nome:")
-ano_nascimento = int(input("Digite o ano em que nasceu:"))
-ano_atual = 2026
+# nome = input("Digite o seu nome:")
+# ano_nascimento = int(input("Digite o ano em que nasceu:"))
+# ano_atual = 2026
 
-idade = ano_atual - ano_nascimento
+# idade = ano_atual - ano_nascimento
 
-print(f"Olá, {nome}!")
-print(f"Você tem {idade} anos.")
+# print(f"Olá, {nome}!")
+# print(f"Você tem {idade} anos.")
 
 # Crie um programa que recebe o nome de um produto, o preço e a quantidade comprada desse produto. Exiba ao final o valor total a pagar desse produto.
+
+# nome_produto = input("Digite o nome do produto: ")
+# preco_produto = float(input("Digite o preço do produto: "))
+# qtd_produto = int(input("Digite quantas unidades deseja comprar: "))
+
+# valor_total = qtd_produto * preco_produto
+
+# print(f"Você comprou {qtd_produto} unidades de {nome_produto} e precisará pagar R$ {valor_total:.2f}")
 
 # Crie um programa que pede o nome de um aluno e suas 4 notas. Ao final exiba a média do aluno.
