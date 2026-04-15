1d458dd96415ba9defefa355f08f58f171247c7f
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-15 18:25:03 -0300

    Aquecimento

---
+++ Aula 6/revisao_operadores_condicao.py
@@ -0,0 +1,29 @@
+"""
+Operadores Condicionais:
+
+== -> Igualdade
+!= -> Desigualdade
+> -> Maior que
+< -> Menor que
+>= -> Maior ou igual
+<= -> Menor ou igual
+
+not -> Inverte a lógica
+
+Conectores:
+
+and -> "E" Precisa que ambas as afirmações sejam verdadeiras
+
+or -> "OU" Precisa que PELO MENOS UMA afirmação seja verdadeira
+
+"""
+
+# 1. Crie um programa que pergunta se uma pessoa tem reserva para o restaurante. Verifique se a resposta foi especificamente "Sim" e imprima o resultado da verificação:
+
+#Ex: "Entrada Permitida: {True/False}"
+
+# 2. Crie um programa que pede um login e senha. Se o login informado for 'admin' e a senha for 'pass' mostre na tela "Acesso Concedido: {True/False}"
+
+# 3. Crie um programa que pede um número inteiro e verifique se ele é par. Imprima na tela o resultado da verificação:
+
+#Ex: "Par: {True/False}"
