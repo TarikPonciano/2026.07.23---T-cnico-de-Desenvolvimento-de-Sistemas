2c6beccf7ef28c827ff640e865bd670ad59e18ba
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-17 20:06:13 -0300

    Exercicios

--- Aula 7/revisaoCondicionais.py
+++
@@ -1,7 +0,0 @@
-# Crie um programa que contém um número secreto à sua escolha (guarde 0 a 10 em uma variável) e peça ao usuário que dê um palpite (0 a 10). Imprima na tela se o usuário acertou ou errou o seu palpite.
-
-# Bônus 1: Melhore o programa para quando o usuário errar imprima se o número secreto é maior ou menor que o palpite
-
-# Bônus 2: Permita que o usuário tente 3 vezes antes de encerrar o programa
-
-# Bônus 3: Faça com que o número secreto seja aleatório
