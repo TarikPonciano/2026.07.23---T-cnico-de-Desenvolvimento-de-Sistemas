04dec51672104a6953ddd745e81abb96947f7201
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-17 20:41:26 -0300

    Create jogoPalpite2.py

---
+++ Aula 7/jogoPalpite2.py
@@ -0,0 +1,33 @@
+num_secreto = 7
+
+palpite = int(input("Digite um número de 0 a 10: "))
+
+if palpite == num_secreto:
+    print("Você acertou!")
+    exit()
+elif num_secreto > palpite:
+    print("Você errou! O número secreto era maior.")
+else:
+    print("Você errou! O número secreto era menor.")
+
+palpite = int(input("Digite um número de 0 a 10: "))
+
+if palpite == num_secreto:
+    print("Você acertou!")
+    exit()
+elif num_secreto > palpite:
+    print("Você errou! O número secreto era maior.")
+else:
+    print("Você errou! O número secreto era menor.")
+
+
+palpite = int(input("Digite um número de 0 a 10: "))
+
+if palpite == num_secreto:
+    print("Você acertou!")
+    exit()
+elif num_secreto > palpite:
+    print("Você errou! O número secreto era maior.")
+else:
+    print("Você errou! O número secreto era menor.")
+    
