d4365d9ac8b8ad222b0995e806802cef5205d47f
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-23 18:27:57 -0300

    Aula 10

---
+++ Aula 10/desafio.py
@@ -0,0 +1,5 @@
+#1. Crie um programa que recebe três números inteiros e imprima o maior deles.
+
+#2. Crie um programa que recebe três números inteiros e imprima eles em ordem crescente.
+
+#3. Crie um programa que pede 10 números inteiros e imprima a média deles.
