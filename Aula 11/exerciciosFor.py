7b4cbad0b87281c9b343bbe64edae6a21b6d3c17
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-24 20:04:56 -0300

    Aula 11

---
+++ Aula 11/exerciciosFor.py
@@ -0,0 +1,15 @@
+# Imprima todos os números de 0 até 1000
+# for i in range(1001):
+#     print(i)
+
+# Imprima todos os números pares de 0 até 1000
+# for i in range(0, 1001,2):
+#     print(i)
+
+for i in range(1,1001):
+    if i % 100 == 0:
+        print(i)
+
+# Imprima a soma dos números pares de 0 até 10000
+
+# Imprima a quantidade de números pares de 0 até 1000
