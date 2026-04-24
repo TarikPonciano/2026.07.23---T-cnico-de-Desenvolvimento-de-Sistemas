01abe78763d1a66eab3519f7968657c6a81e10ec
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-24 20:17:16 -0300

    Update exerciciosFor.py

--- Aula 11/exerciciosFor.py
+++ Aula 11/exerciciosFor.py
@@ -10,6 +10,6 @@ for i in range(1,1001):
     if i % 100 == 0:
         print(i)
 
-# Imprima a soma dos números pares de 0 até 10000
+# Imprima a soma dos números pares de 0 até 1000
 
 # Imprima a quantidade de números pares de 0 até 1000
