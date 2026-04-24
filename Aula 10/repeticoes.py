17bc62417e024371b010b114d4be7af9556374c6
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-23 21:17:45 -0300

    Inicio For

---
+++ Aula 10/repeticoes.py
@@ -0,0 +1,24 @@
+# Altere o programa abaixo para, pedir o nome de uma pessoa e imprimir "Hello, {nome}" 5 vezes.
+
+# Melhore o programa perguntando quantas vezes a pessoa deseja repetir. E imprima "Hello, {nome}" de acordo com o número que a pessoa escreveu
+# nome = input("Digite o seu nome: ")
+# repeticoes = int(input("Digite quantas vezes deseja repetir seu nome: "))
+
+# for i in range(repeticoes):
+#     print(f"Hello, {nome}")
+
+
+
+# Faça um programa que repete 5 vezes a seguinte operação: Pede o nome da pessoa e imprime na tela "Olá, {nome}. Seja bem vindo."
+
+# for i in range(5):
+#     nome = input("Digite o seu nome: ")
+
+#     print(f"Olá, {nome}. Seja bem vindo.")
+
+
+# Faça um programa que imprime os números de 0 a 500
+for i in range(501):
+    print(i)
+
+# Faça um programa que imprime a soma dos números de 0 a 500
