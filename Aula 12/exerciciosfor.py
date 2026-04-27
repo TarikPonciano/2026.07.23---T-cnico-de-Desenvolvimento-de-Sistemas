8519fb1e9173f4e02e112922427cee95e39cc2e7
professoreugenio <professoreugeniomls@gmail.com>
2026-04-27 18:54:23 -0300

    Create exerciciosfor.py

---
+++ Aula 12/exerciciosfor.py
@@ -0,0 +1,12 @@
+# 1. Peça um número inteiro ao usuário, na sequência exiba na tela a tabuada desse número (multiplicações de 0 a 10)
+
+# 2. Faça um programa que repete 4 vezes a seguinte operação: Pede uma nota de um aluno e acrescenta a nova nota à soma das notas. Ao final das repetições exiba a média do aluno e se ele foi aprovado, reprovado ou está de recuperação.(Defina as notas para cada condição)
+
+# Bônus: Considere apenas notas entre 0 e 10.
+
+# 3. Faça um programa que recebe um palpite de um usuário e verifica se esse palpite é igual o número secreto(usar um número aleatório). Permita que o jogador tenha até 3 tentativas.
+# Bônus: Faça com o que o jogo encerre assim que o jogador acertar o número secreto.
+# Bônus: Faça com que o jogo imprima uma mensagem especial para quando o número de tentativas chegar ao final.
+
+# 4. Receba o nome, o preço e a quantidade de 5 produtos diferentes. Ao final exiba o valor total que o cliente deverá pagar.
+# Bônus: Exiba uma nota fiscal com tudo que foi comprado e as informações da venda ao final.
