9bbbb815e06aaad3cb7a961435378212473e325e
professoreugenio <40394108+TarikPonciano@users.noreply.github.com>
2026-04-27 19:15:26 -0300

    Questão 1

--- Aula 12/exerciciosfor.py
+++ Aula 12/exerciciosfor.py
@@ -1,8 +1,11 @@
 # 1. Peça um número inteiro ao usuário, na sequência exiba na tela a tabuada desse número (multiplicações de 0 a 10)
 
-# 2. Faça um programa que repete 4 vezes a seguinte operação: Pede uma nota de um aluno e acrescenta a nova nota à soma das notas. Ao final das repetições exiba a média do aluno e se ele foi aprovado, reprovado ou está de recuperação.(Defina as notas para cada condição)
+# 2. Faça um programa que repete 4 vezes a seguinte operação: Pede uma nota de um aluno e acrescenta a nova nota à soma das notas. 
+# Ao final das repetições exiba a média do aluno e se ele foi aprovado, reprovado ou está de recuperação.(Defina as médias para cada condição)
 
 # Bônus: Considere apenas notas entre 0 e 10.
+# Bônus: Permita que o professor decida quantas notas serão registradas.
+# Bônus: Exiba o boletim do aluno ao final
 
 # 3. Faça um programa que recebe um palpite de um usuário e verifica se esse palpite é igual o número secreto(usar um número aleatório). Permita que o jogador tenha até 3 tentativas.
 # Bônus: Faça com o que o jogo encerre assim que o jogador acertar o número secreto.
