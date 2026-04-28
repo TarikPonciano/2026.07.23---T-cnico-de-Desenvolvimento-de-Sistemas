0955c852c9aa141661be939e9dd1d067094a63ca
professoreugenio <40394108+TarikPonciano@users.noreply.github.com>
2026-04-28 18:04:33 -0300

    Create exerciciosfor.py

---
+++ Aula 13/exerciciosfor.py
@@ -0,0 +1,9 @@
+# 1. Receba 10 números inteiros. Imprima na tela a soma dos números e quantos eram números pares.
+
+# 2. Crie um programa que gera bilhetes da mega-sena. O programa deve gerar 6 números inteiros entre 1 e 60 e imprimi-los no formato:
+# num1 - num2 - num3 - num4 - num5 - num6
+# Bônus: Evite que um mesmo número seja impresso múltiplas vezes
+
+# 3. Crie um programa que recebe 5 números reais, exiba na tela o maior entre os números inseridos.
+
+# 4. Uma estação de metereologia contratou você para criar um programa que calcula a média de temperatura de um dia. O seu programa deve perguntar quantas temperaturas serão inseridas, fazer a leitura dessas temperaturas e ao final informar qual foi a temperatura média daquele dia, a temperatura máxima e a temperatura mínima do dia.
