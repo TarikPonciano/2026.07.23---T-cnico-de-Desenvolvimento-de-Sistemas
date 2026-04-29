6e553823907ee5d257e24d73b0c89a03965d25ac
professoreugenio <40394108+TarikPonciano@users.noreply.github.com>
2026-04-29 19:55:27 -0300

    Update exercicioswhile.py

--- Aula 14/exercicioswhile.py
+++ Aula 14/exercicioswhile.py
@@ -2,4 +2,7 @@
 
 # 2. Crie um programa que pede números inteiros até que seja digitado -1. Quando o usuário digitar -1 exiba a soma de todos os números
 
-# 3. Em um campeonato de arquearia o jogador pode pontuar até que erre o alvo. Registre os acertos de um jogador (1 a 20 pontos) e encerre sua rodada quando atingir 0 pontos
+# 3. Em uma competição de corrida um programa deve receber quantos metros cada competidor correu. Execute o programa até que seja inserido 0 na quantidade de metros percorridos. Ao final mostre qual foi o maior percurso registrado.
+# Bônus: Colete também o nome de cada competidor e exiba o nome do competidor com o maior percurso
+
+# 4. Crie um programa que pede o nome dos funcionários de um departamente até que seja fornecido um funcionário com o nome "SAIR". Exiba quantos funcionários foram cadastrados.
