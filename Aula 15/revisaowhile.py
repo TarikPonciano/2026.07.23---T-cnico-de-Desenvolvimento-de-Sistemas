5d489a9a7bc28f19a9d2fac066a907ea14c5cd48
professoreugenio <40394108+TarikPonciano@users.noreply.github.com>
2026-04-30 19:01:54 -0300

    Create revisaowhile.py

---
+++ Aula 15/revisaowhile.py
@@ -0,0 +1,36 @@
+# idade = 0
+
+# while idade < 18:
+#     idade = int(input("Digite sua idade:"))
+
+#     if idade < 18:
+#         print("Você é menor de idade, digite sua idade novamente.")
+
+# print("Você saiu do loop pois é maior de idade")
+
+
+while True:
+
+    idade = int(input("Digite sua idade: "))
+
+    if idade < 18:
+        print("Você é menor de idade. Acesso Negado. Digite idade novamente.")
+    else:
+        break
+
+print("Bem vindo ao Programa")
+
+# Crie uma calculadora onde a pessoa informa dois números e um operação (+,-,*,/). Ao terminar um cálculo exiba o resultado e pergunte se a pessoa deseja realizar outro cálculo. Caso a resposta seja sim, peça novamente os dois números e operação, caso a pessoa não deseje continuar encerre o programa.
+
+# Faça um menu de atendimento eletrônico que contenham as opções:
+
+# 1. Falar com atendente
+# 2. Finalizar contrato
+# 3. Abrir nova conta
+# 4. Visualizar segunda via da fatura
+# 0. Sair
+
+# Cada opção deve exibir uma mensagem relevante. Ao ver a mensagem, volte para o menu e peça uma nova opção do usuário. Se a pessoa escolher a opção Sair, encerre o programa. Caso a pessoa escreva uma opção inválida exiba uma mensagem de erro e volte para o menu.
+
+# Crie um sistema escolar onde para calcular a média de um aluno devemos receber 4 notas válidas (entre 0 e 10). Ao final exiba o status de aprovação do aluno.
+
