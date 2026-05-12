81067d925fd1dc0663a76505627634e837fc24df
professoreugenio <40394108+TarikPonciano@users.noreply.github.com>
2026-05-12 19:39:30 -0300

    Questões

---
+++ Aula 21/questoesdicionarios.py
@@ -0,0 +1,5 @@
+# Faça um programa que pede as informações nome, espécie, peso e idade de um animal de atendimento veterinário. Salve essas informações em um dicionário e exiba a ficha do paciente.
+
+# Crie um programa que guarda as pontuações do brasileirão. Para guardar essas pontuações use um dicionário onde a chave será o nome do time e o valor será a pontuação do time. Crie pelo menos 5 entradas.
+
+# Crie uma agenda telefonica onde o telefone da pessoa deve ser acessado pelo seu nome. Você deve usar um dicionário onde a chave será o nome e o valor será o telefone. Peça para que a pessoa escreva um nome e mostre na tela o telefone da pessoa escolhida. Faça pelo menos 10 contatos.
