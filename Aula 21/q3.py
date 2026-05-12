0ccfa014103ec4a03a30dfe2050fb9f2b708c417
professoreugenio <40394108+TarikPonciano@users.noreply.github.com>
2026-05-12 20:56:21 -0300

    Q3

--- Aula 21/q3.py
+++ Aula 21/q3.py
@@ -1,12 +1,5 @@
 # Crie uma agenda telefonica onde o telefone da pessoa deve ser acessado pelo seu nome. Você deve usar um dicionário onde a chave será o nome e o valor será o telefone. Peça para que a pessoa escreva um nome e mostre na tela o telefone da pessoa escolhida. Faça pelo menos 10 contatos.
 
-# Utilizando a agenda do problema anterior, realize as seguintes melhorias no programa:
-
-# 1 - Peça para o usuário o nome e telefone de 5 novas pessoas e adicione na agenda.
-
-# 2 - Utilize um loop while para permitir que a pessoa consulte os números da agenda. O usuário deverá escrever o nome de um dos contatos e o programa deve exibir o número na tela (caso exista, senão exibir mensagem de erro) e depois pedir o nome do próximo. Continue até que a pessoa escreva o nome "Sair".
-# 3 - No começo de cada repetição, exiba a lista de contatos na tela.
-
 
 agenda = {
     "Julia": "8599664433",
