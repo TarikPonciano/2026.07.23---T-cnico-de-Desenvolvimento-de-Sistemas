38d4113fa6bb2322fbb533fb379a6384df5badff
professoreugenio <40394108+TarikPonciano@users.noreply.github.com>
2026-05-13 19:07:33 -0300

    Update agendaTelefonica.py

--- Aula 21/agendaTelefonica.py
+++ Aula 21/agendaTelefonica.py
@@ -5,17 +5,39 @@
 # 1 - Peça para o usuário o nome e telefone de 5 novas pessoas e adicione na agenda.
 
 # 2 - Utilize um loop while para permitir que a pessoa consulte os números da agenda. O usuário deverá escrever o nome de um dos contatos e o programa deve exibir o número na tela (caso exista, senão exibir mensagem de erro) e depois pedir o nome do próximo. Continue até que a pessoa escreva o nome "Sair".
+
 # 3 - No começo de cada repetição, exiba a lista de contatos na tela.
 
 
 agenda = {
     "Julia": "8599664433",
     "Marcos": "8598764532",
-    "Lopes": "8588745423"
+    "Lopes": "8588745423",
+    "Getúlio": "8599123456"
 }
-nome = input("Digite o nome da pessoa que você deseja visualizar o telefone: ")
 
-if nome in agenda:  
-    print(agenda[nome])
-else:
-    print("Nome não encontrado!")
+for i in range(5):
+    nome = input("Digite o nome o do contato: ")
+    telefone = input("Digite o telefone do contato: ")
+    agenda[nome] = telefone
+
+while True:
+    print("Lista de Contatos")
+    for n in agenda:
+        print(f"{n} - {agenda[n]}")
+
+    nome = input("Digite o nome do contato que deseja visualizar:")
+
+    if nome.upper() == "SAIR":
+        print("Encerrando app agenda...")
+        break
+
+    print(f"{nome}: {agenda.get(nome,"Nome não encontrado!")}")
+
+    input("DIGITE ENTER PARA CONTINUAR")
+
+    # if nome in agenda:
+    #     print(f"{nome}: {agenda[nome]}")
+    # else:
+    #     print("Nome não encontrado!")
+
