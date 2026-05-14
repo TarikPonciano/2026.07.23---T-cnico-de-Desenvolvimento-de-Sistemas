ac17d6d1a9b22d503be656a768fb29e5d98a3ef3
professoreugenio <40394108+TarikPonciano@users.noreply.github.com>
2026-05-14 20:02:54 -0300

    Update sistema.py

--- Aula 23/sistema.py
+++ Aula 23/sistema.py
@@ -26,10 +26,41 @@ Menu de Opções:
     if op == "1":
         # Crie um sistema de cadastro onde o usuário preenche as informações do funcionário e na sequência armazena esse funcionário no sistema.
         #Informações do funcionário serão Nome, CPF, Cargo, Salário e Departamento
-        print("SISTEMA DE CADASTRO")
+        #Valide se o cpf tem 11 caracteres
+        print("CADASTRO DE FUNCIONÁRIO")
+
+        nome = input("Digite o nome: ")
+
+        while True:
+            cpf = input("Digite o cpf com 11 caracteres: ")
+        
+            # if len(cpf) != 11 or not cpf.isdigit():
+            #     print("CPF INVÁLIDO!")
+            # else:
+            #     break
+
+            break
+
+        cargo = input("Digite o cargo: ")
+        salario = float(input("Digite o salário: "))
+        departamento = input("Digite o departamento: ")
+
+        novo_funcionario = {
+            "Nome": nome,
+            "CPF": cpf,
+            "Cargo": cargo,
+            "Salário": salario,
+            "Departamento": departamento
+        }
+
+        funcionarios.append(novo_funcionario)
+
     elif op == "2":
+        # Imprimir a lista de funcionários de forma numerada exibindo apenas o seu nome
         print("SISTEMA DE VISUALIZAÇÃO")
+        print(funcionarios)
     elif op == "3":
+        # Exibir também a lista de funcionários, porém deveremos perguntar ao usuário qual funcionário queremos ver mais detalhes. Após escolher um funcionário usando o número, exibir a ficha formatada do funcionário.
         print("ESCOLHA UM FUNCIONÁRIO!")
     elif op == "0":
         print("ENCERRANDO PROGRAMA...")
