d37e77609346906a6478187a6e8b4d742386a24b
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2026-06-02 20:36:03 -0300

    Testes Instaciamentos

---
+++ Aula 8/Testes Objetos/classPokemon.py
@@ -0,0 +1,5 @@
+class Pokemon():
+    def __init__(self, especie, tipo, level):
+        self.especie = especie
+        self.tipo = tipo
+        self.level = level
--- Aula 8/Testes Objetos/main.py
+++ Aula 8/Testes Objetos/main.py
@@ -1,7 +1,43 @@
 from classAluno import Aluno
+from classLivro import Livro
+from classPokemon import Pokemon
 
 aluno1 = Aluno("Joaquim", 32, 1, [7,8,8.5,9], "A")
 
 aluno2 = Aluno("Manoel", 35, 2, [8,8,9,9], "B")
 
-print(aluno2.nome)
+print(f"""
+Ficha do Aluno:
+      
+      Nome: {aluno1.nome}
+      Idade: {aluno1.idade}
+      Matricula: {aluno1.matricula}
+      Notas: {aluno1.notas}
+      Turma: {aluno1.turma}
+
+""")
+
+livro1 = Livro("Crônicas de Nárnia", "J.k Lewis", "Fantasia")
+
+print(f"""
+
+FICHA DO LIVRO:
+
+Título: {livro1.titulo}
+Autor: {livro1.autor}
+Genero: {livro1.genero}
+    
+      """)
+
+
+poke1 = Pokemon("Pikachu", "Elétrico", 24)
+
+print(f"""
+
+POKEDEX:
+      
+Espécie: {poke1.especie}
+Tipo: {poke1.tipo}
+Level: {poke1.level}
+
+""")
